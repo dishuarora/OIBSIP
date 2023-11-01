9e9ac4651be72e2824a4772185b6ca384a5e4f22
dishuarora <dikshaoibsip12@gmail.com>
2023-11-02 02:01:05 +0530

    first commit

---
+++ random.js
@@ -0,0 +1,20 @@
+let jokeContainer = document.getElementById("joke");
+
+let btn = document.getElementById("btn");
+
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let getJoke = () => {
+   fetch(url)
+   .then(data => data.json())
+   .then(item => {
+      jokeContainer.textContent = `${item.joke}`;
+
+   });
+}
+btn.addEventListener("click",getJoke);
+  
+
+
+ 
+ getJoke();
